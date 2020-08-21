f8f76bbefcd6a7e7d4518a74ab7fe4011829f744
Rayaa <rghirri@gmail.com>
2020-08-21 10:42:40 +0100

    First Commit

---
+++ javascript.js
@@ -0,0 +1,6 @@
+var number_of_children = 4;
+var Partner = 'James Bond';
+var geographic_location = 'London';
+var job_title= 'Gardener';
+document.getElementById("demo").innerHTML =
+"You will be a " +job_title+" in "+geographic_location+' , and married to '+Partner+' with '+number_of_children+' kids.';
